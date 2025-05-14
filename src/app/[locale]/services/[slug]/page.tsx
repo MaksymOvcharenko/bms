67e6114389d12e90611983857875b57c6eac309d
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2025-05-14 21:05:26 +0200

    change style uslugi

--- src/app/[locale]/services/[slug]/page.tsx
+++ src/app/[locale]/services/[slug]/page.tsx
@@ -3,7 +3,11 @@
 import { client } from "@/lib/contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { Document } from "@contentful/rich-text-types";
-
+import s from "./styles.module.css"
+import Image from "next/image";
+import ContactInfoSection from "@/components/ContactInfoSection/ContactInfoSection";
+import SocialLinks from "@/components/SocialLinks/SocialLinks";
+import FollowSocLink from "@/components/FollowSocLink/FollowSocLink";
 const localeMap: Record<string, string> = {
   uk: "uk-UA",
   pl: "pl-PL",
@@ -48,9 +52,24 @@ export default async function ServicePage({
   if (!service) return <h1>Послугу не знайдено</h1>;
 
   return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">{service.fields.title}</h1>
-      <div>{documentToReactComponents(service.fields.contents as Document)}</div>
-    </div>
+    <>
+      <div className={s.container}>
+        <div className={s.content}>
+          <h1 className="text-3xl font-bold mb-4">{service.fields.title}</h1>
+          <div className={s.imageContainer}>
+            <Image
+              src={`https:${service.fields.image.fields.file.url}`}
+              alt={service.fields.image.fields.title}
+              className={s.image}
+              width={416}
+              height={224}
+            />
+          </div>
+          <div>{documentToReactComponents(service.fields.contents as Document)}</div>
+        </div >
+      </div>
+      <ContactInfoSection />
+      <FollowSocLink/>
+    </>
   );
 }
