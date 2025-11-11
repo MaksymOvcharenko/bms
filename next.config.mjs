895a015983343b3024cd38d6d1fa85a6c07b1e8c
MaksymOvcharenko <153368666+MaksymOvcharenko@users.noreply.github.com>
2025-11-11 16:03:58 +0100

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,42 @@
-// // import createNextIntlPlugin from 'next-intl/plugin';
-
-// // const withNextIntl = createNextIntlPlugin();
-
-// // /** @type {import('next').NextConfig} */
-// // const nextConfig = {};
-
-// // export default withNextIntl(nextConfig);
-// import createNextIntlPlugin from "next-intl/plugin";
+// import createNextIntlPlugin from 'next-intl/plugin';
 
 // const withNextIntl = createNextIntlPlugin();
 
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["images.ctfassets.net"],
-//   },
-// };
+// const nextConfig = {};
 
 // export default withNextIntl(nextConfig);
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["images.ctfassets.net"],
   },
-
-  // ✅ 301 редірект з /pl → /
-  async redirects() {
-    return [
-      {
-        source: "/pl/:path*", // усе, що починається з /pl
-        destination: "/:path*", // перекидаємо на корінь
-        permanent: true, // 301 redirect (для SEO)
-      },
-    ];
-  },
 };
 
 export default withNextIntl(nextConfig);
+// import createNextIntlPlugin from "next-intl/plugin";
+
+// const withNextIntl = createNextIntlPlugin();
+
+// const nextConfig = {
+//   images: {
+//     domains: ["images.ctfassets.net"],
+//   },
+
+//   // ✅ 301 редірект з /pl → /
+//   async redirects() {
+//     return [
+//       {
+//         source: "/pl/:path*", // усе, що починається з /pl
+//         destination: "/:path*", // перекидаємо на корінь
+//         permanent: true, // 301 redirect (для SEO)
+//       },
+//     ];
+//   },
+// };
+
+// export default withNextIntl(nextConfig);
